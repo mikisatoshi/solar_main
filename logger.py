31b0087b28818442c7f537d3a90c852696bcef8d
DESKTOP-DELRI4U\kuros <mikisatoshi1992@gmail.com>
2019-11-30 22:38:41 +0900

    no message

--- logger.py
+++ logger.py
@@ -26,6 +26,7 @@ class PiLoger():
     import bme280_   
     self.init_get_adc_data()
     value = self.adc.read_adc(0, gain=self.GAIN)
+    value = read_adc_difference(0, gain=self.GAIN)
 
     dt_now = datetime.datetime.now()
     # value = dt_now.hour * 60 + dt_now.minute
@@ -35,7 +36,6 @@ class PiLoger():
 
   def init_get_adc_data(self):
     self.adc = ADS1x15.ADS1115()
-    self.counter_adc = 0
     self.GAIN = 1
 
 
