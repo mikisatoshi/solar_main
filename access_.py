e17b33ad758198c81505c557422cee5256534661
DESKTOP-DELRI4U\kuros <mikisatoshi1992@gmail.com>
2019-12-01 15:20:06 +0900

    no message

--- access_.py
+++ access_.py
@@ -20,7 +20,7 @@ class PiAccess():
   def get_latest_values(self, number = 1):# example "A17:D10000"
     print(self.worksheet.acell('A35').value)
     last_row_number = self.worksheet.acell('A35').value
-    print(self.worksheet.acell('A' + str(last_row_number +1 -number)).value)
+    print(self.worksheet.acell('A' + str(int(last_row_number +1 -number))).value)
     print('A' + str(int(last_row_number +1 -number)) + ':F' + str(int(last_row_number +1 -number)) )
     # print(self.worksheet.row_count)
     # print(self.worksheet.findall('test'))
