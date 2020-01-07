493075249697a4fbecc529405c4197a31637d37b
DESKTOP-DELRI4U\kuros <mikisatoshi1992@gmail.com>
2020-01-08 00:00:52 +0900

    no message

--- logger.py
+++ logger.py
@@ -122,21 +122,21 @@ if __name__ == '__main__':
 
 
   if para['logger'] == 'on':
-    # try:
-    print(' try  logger   ')
-    main(para)
-    print('   succes   ')
-    # except:
-    print('  para[logger] == on  :  failed   ')
+    try:
+      print(' try  logger   ')
+      main(para)
+      print('   succes   ')
+    except:
+      print('  para[logger] == on  :  failed   ')
 
 
   if para['make_hour_report'] == 'on':
-    try:
-      print(' try  make_hour_report   ')
-      make_hour_report(para)
-      print('   succes   ')
-    except:
-      print('  para[make_hour_report] == on  :  failed   ')
+    # try:
+    print(' try  make_hour_report   ')
+    make_hour_report(para)
+    print('   succes   ')
+    # except:
+    print('  para[make_hour_report] == on  :  failed   ')
 
 
 
